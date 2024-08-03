a5a3538ad21cda26f9597f3402ee577498c5416b
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-08-03 18:55:12 +0500

    Started learning react JS

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+  //  const [name, setName] = useState(0)
+  
+  // const handleName = ()=>{
+  //   setName(name + 1)
+  // }
+  // console.log(name);
+
+
+  // function MyButton() {
+  //   function handleClick() {
+  //     alert('You clicked me!');
+  //   }
+  
+  //   return (
+  //     <button onClick={handleClick}>
+  //       Click me
+  //     </button>
+  //   );
+  // }
+  // function MyButton(){
+  //   function handleClick() {
+  //     alert ('you clicked me!')
+  //   }
+  //   return(
+  //     <button onClick={handleClick}>click me 
+
+  //     </button>
+  //   )
+  // }
+
+
+
+
+  
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={handleName}>
+  //         count is {name}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.jsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
+// }
+
+export default MyButton
